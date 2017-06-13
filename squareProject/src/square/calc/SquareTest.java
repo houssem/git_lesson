3fce75af3d5ee5306b7eb2843bcdd823aa578968
Houssem <houssem.kallel@gmail.com>
2017-06-13 11:39:08 +0100

    square project : initial commit

---
+++ squareProject/src/square/calc/SquareTest.java
@@ -0,0 +1,16 @@
+package square.calc;
+
+public class SquareTest {
+
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			Integer value = Integer.valueOf(args[0]);
+			System.out.println(value * value);
+		} else {
+			System.out.println("No value introduced");
+		}
+
+	}
+
+}
